ee50a4f750f7258ca5c23dd63649861d08607ea9
Shep Moore-Berg <mooreberg@g.harvard.edu>
2016-09-14 22:33:55 -0400

    Made form prettier and centered.

--- index.php
+++ index.php
@@ -12,47 +12,67 @@
     <body>
 
         <?php include_once("header.html"); ?>
+        
 
+        <h4 class="text-center">put-generated-passsword-here</h4>
+           
 
+        <div class="row">
+            <div class="small-6 small-centered medium-6 large-4 large-centered columns">
                 <form>
-                    <div class="row text-center">
-                        <div class="small-2 small-centered columns">
-                            <label>Separator
-                                <select>
-                                    <option value="space">Space</option>
-                                    <option value="hyphen">Hyphen</option>
-                                    <option value="period">Period</option>
-                                    <option value="underscore">Underscore</option>
-                                    <option value="comma">Comma</option>
-                                    <option value="camel">None (camelCase)</option>
-                                </select>
-                            </label>
+                    <div class="row column log-in-form">
+
+                        <div class="row">
+                            <label>Number of words</label>
+                            <div class="slider small-8 columns" data-slider data-initial-start="5" data-start="1" data-end="9">
+                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-words"></span>
+                                    <span class="slider-fill" data-slider-fill></span>
+                            </div>
+                            <div class="small-4 columns">
+                                <input type="number" id="num-words" placeholder="Number of words">
+                            </div>
                         </div>
-                    </div>  
-                    <div class="row text-center">
-                        <label>Choose number of words
-                            <div class="small-1 small-centered columns">
-                                <input type="number" id="num-words" max="9" min="4">
+
+                        <div class="row">
+                            <label>Number of symbols at end</label>
+                            <div class="slider small-8 columns" data-slider data-initial-start="5" data-start="1" data-end="9">
+                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-symbols"></span>
+                                    <span class="slider-fill" data-slider-fill></span>
                             </div>
-                        </label>
-                    </div>
-                    <div class="row text-center">
-                        <label>Choose number of symbols
-                            <div class="small-1 small-centered columns">
-                                <input type="number" id="num-symbols" max="9" min="1">
+                            <div class="small-4 columns">
+                                <input type="number" id="num-symbols" placeholder="Number of words">
                             </div>
-                        </label>
-                    </div>
-                    <div class="row text-center">
-                        <label>Choose number of numbers
-                            <div class="small-1 small-centered columns">
-                                <input type="number" id="num-numbers" max="9" min="1">
+                        </div>
+
+                        <div class="row">
+                            <label>Number of numbers at end</label>
+                            <div class="slider small-8 columns" data-slider data-initial-start="5" data-start="1" data-end="9">
+                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-numbers"></span>
+                                    <span class="slider-fill" data-slider-fill></span>
                             </div>
-                        </label>
+                            <div class="small-4 columns">
+                                <input type="number" id="num-numbers" placeholder="Number of words">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <label>Separator</label>
+                            <select>
+                                <option value="space">Space</option>
+                                <option value="comma">Comma</option>
+                                <option value="underscore">Underscore</option>
+                                <option value="period">Period</option>
+                                <option value="camel">camelCase</option>
+                            </select>
+                        </div>
+                        
+                        <p><button type="submit" class="button expanded" value="Generate Password">Generate Passwords</button></p>
+                        <p class="text-center"><a href="#">Inspiration?</a></p>   
                     </div>
                 </form>
-            
-        
+            </div>
+        </div>
+
 
         <script src="js/vendor/jquery.js"></script>
         <script src="js/vendor/what-input.js"></script>
