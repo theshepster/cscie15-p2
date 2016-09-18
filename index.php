ce9cf50aafbfde29aeeaf13753ce96043ab089ec
Shep Moore-Berg <mooreberg@g.harvard.edu>
2016-09-18 16:54:38 -0400

    Form submission now works

--- index.php
+++ index.php
@@ -14,50 +14,48 @@
         <?php include_once("header.html"); ?>
         
 
-        <h4 class="text-center">put-generated-passsword-here</h4>
-           
 
         <div class="row">
             <div class="small-6 small-centered medium-6 large-4 large-centered columns">
-                <form>
-                    <div class="row column log-in-form">
+                <form method='POST' action='index.php'>
+                    <div class="row column">
 
                         <div class="row">
                             <label>Number of words</label>
                             <div class="slider small-8 columns" data-slider data-initial-start="5" data-start="1" data-end="9">
-                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-words"></span>
+                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-words" aria-valuenow="5" aria-valuemin="1" aria-valuemax="9"></span>
                                     <span class="slider-fill" data-slider-fill></span>
                             </div>
                             <div class="small-4 columns">
-                                <input type="number" id="num-words" placeholder="Number of words">
+                                <input type="number" id="num-words" name="num-words" placeholder="Number of words">
                             </div>
                         </div>
 
                         <div class="row">
                             <label>Number of symbols at end</label>
                             <div class="slider small-8 columns" data-slider data-initial-start="5" data-start="1" data-end="9">
-                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-symbols"></span>
+                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-symbols" aria-valuenow="5" aria-valuemin="1" aria-valuemax="9"></span>
                                     <span class="slider-fill" data-slider-fill></span>
                             </div>
                             <div class="small-4 columns">
-                                <input type="number" id="num-symbols" placeholder="Number of words">
+                                <input type="number" id="num-symbols" name="num-symbols" placeholder="Number of words">
                             </div>
                         </div>
 
                         <div class="row">
                             <label>Number of numbers at end</label>
                             <div class="slider small-8 columns" data-slider data-initial-start="5" data-start="1" data-end="9">
-                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-numbers"></span>
+                                    <span class="slider-handle"  data-slider-handle role="slider" tabindex="1" aria-controls="num-numbers" aria-valuenow="5" aria-valuemin="1" aria-valuemax="9"></span>
                                     <span class="slider-fill" data-slider-fill></span>
                             </div>
                             <div class="small-4 columns">
-                                <input type="number" id="num-numbers" placeholder="Number of words">
+                                <input type="number" id="num-numbers" name="num-numbers" placeholder="Number of words">
                             </div>
                         </div>
 
                         <div class="row">
                             <label>Separator</label>
-                            <select>
+                            <select name="separator">
                                 <option value="space">Space</option>
                                 <option value="comma">Comma</option>
                                 <option value="underscore">Underscore</option>
@@ -66,12 +64,16 @@
                             </select>
                         </div>
                         
-                        <p><button type="submit" class="button expanded" value="Generate Password">Generate Passwords</button></p>
+                        <p><input type="submit" class="button expanded" value="Generate Password"></p>
                         <p class="text-center"><a href="http://xkcd.com/936/">Inspiration?</a></p>   
                     </div>
                 </form>
             </div>
         </div>
+
+
+        <h4 class="text-center"><pre><?php print_r($_POST); ?></pre></h4>
+
 
 
         <script src="js/vendor/jquery.js"></script>
